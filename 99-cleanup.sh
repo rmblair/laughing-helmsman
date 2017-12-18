aeef10fe575d6235fe27dd07599ff405a6ed107f
Ryan Blair <rblair@gmail.com>
2017-12-18 00:13:36 -0800

    add a quick way to pave the cluster

---
+++ 99-cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source vars.sh
+
+for node in ${!instances[@]} ${!controllers[@]}; do
+  kubectl --kubeconfig /etc/kubernetes/admin.conf drain ${node} --delete-local-data --force --ignore-daemonsets
+  kubectl --kubeconfig /etc/kubernetes/admin.conf delete node ${node}
+  ssh root@${node} "kubeadm reset"
+done
